5f57a1a2b8c7dba7c618af55369628e775f4572a
Maxoos <max.penson@gmail.com>
2019-09-18 15:55:55 +1000

    Update deploy.rb

--- rails_master_key/recipes/deploy.rb
+++ rails_master_key/recipes/deploy.rb
@@ -3,10 +3,10 @@ node[:deploy].each do |application, deploy|
   current_path = deploy[:current_path]
   rails_master_key = node[:deploy][application][:environment]["RAILS_MASTER_KEY"]
   
-  Chef::Log.info("create master key for #{rails_env}: #{node[:deploy].to_s}")
+  Chef::Log.info("create master key for #{rails_env}")
   
   execute 'create_master_key' do
     user 'deploy'
-    command "mkdir -p #{current_path}/config/credentials && echo #{rails_master_key} > #{current_path}/config/credentials/#{rails_env}.key"
+    command "mkdir -p config/credentials && echo #{rails_master_key} > config/credentials/#{rails_env}.key"
   end
 end
