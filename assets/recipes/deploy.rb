2fdd3d174e934a75e0a8a9285cd36a111f83948c
Maxoos <max.penson@gmail.com>
2019-09-18 12:26:16 +1000

    Update deploy.rb

--- assets/recipes/deploy.rb
+++ assets/recipes/deploy.rb
@@ -3,6 +3,14 @@ node[:deploy].each do |application, deploy|
   current_path = deploy[:current_path]
   rails_master_key = deploy[:rails_master_key]
 
+  Chef::Log.info("create master key for #{rails_env}")
+  
+  execute 'create_master_key' do
+    cwd current_path
+    user 'deploy'
+    command "echo #{rails_master_key} > /config/credentials/#{rails_env}.key"
+  end
+  
   Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
 
   execute 'rake assets:precompile' do
@@ -10,12 +18,6 @@ node[:deploy].each do |application, deploy|
     user 'deploy'
     command "bundle exec rake assets:precompile RAILS_ENV=#{rails_env}"
     environment 'RAILS_ENV' => rails_env
-  end
-  
-  execute 'create_master_key' do
-    cwd current_path
-    user 'deploy'
-    command "echo #{rails_master_key} > /config/credentials/#{rails_env}.key"
   end
   
 end
