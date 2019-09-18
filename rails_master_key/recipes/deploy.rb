2db4fdf6905537a1869ef7d5ca3ee9cd10028a66
Maxoos <max.penson@gmail.com>
2019-09-18 13:23:20 +1000

    Update deploy.rb

--- rails_master_key/recipes/deploy.rb
+++ rails_master_key/recipes/deploy.rb
@@ -1,8 +1,8 @@
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   current_path = deploy[:current_path]
-  rails_master_key = application[:environment][:rails_master_key]
-
+  rails_master_key = deploy['cubbi'][:environment_variables][:rails_master_key]
+  
   Chef::Log.info("create master key for #{rails_env}")
   
   execute 'create_master_key' do
