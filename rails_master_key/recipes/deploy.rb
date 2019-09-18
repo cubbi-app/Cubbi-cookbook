26f570eaa25d1dd493cd58a19a4b76ab7541d4cb
Maxoos <max.penson@gmail.com>
2019-09-18 12:34:58 +1000

    Create deploy.rb

---
+++ rails_master_key/recipes/deploy.rb
@@ -0,0 +1,11 @@
+  node[:deploy].each do |application, deploy|
+  rails_master_key = deploy[:rails_master_key]
+
+  Chef::Log.info("create master key for #{rails_env}")
+  
+  execute 'create_master_key' do
+    cwd current_path
+    user 'deploy'
+    command "echo #{rails_master_key} > /config/credentials/#{rails_env}.key"
+  end
+end
