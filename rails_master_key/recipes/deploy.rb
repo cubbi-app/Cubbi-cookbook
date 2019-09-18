bebf3ae72391eab0ee32f79142717c2672eb2478
Maxoos <max.penson@gmail.com>
2019-09-18 12:44:38 +1000

    Update deploy.rb

--- rails_master_key/recipes/deploy.rb
+++ rails_master_key/recipes/deploy.rb
@@ -1,4 +1,6 @@
 node[:deploy].each do |application, deploy|
+  rails_env = deploy[:rails_env]
+  current_path = deploy[:current_path]
   rails_master_key = deploy[:rails_master_key]
 
   Chef::Log.info("create master key for #{rails_env}")
